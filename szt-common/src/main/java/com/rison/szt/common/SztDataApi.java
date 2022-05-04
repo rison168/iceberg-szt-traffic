eab2b580b0784a14affb73bb2a63ec293aedb17b
Rison Lee <rison168@163.com>
2022-05-04 23:45:59 +0800

    [feat] get web:[https://opendata.sz.gov.cn] SZT ods data

---
+++ szt-common/src/main/java/com/rison/szt/common/SztDataApi.java
@@ -0,0 +1,16 @@
+package com.rison.szt.common;
+
+
+public class SztDataApi{
+    /**
+     * 数据保存路径
+     */
+    final static String DATA_PATH = "";
+    /**
+     * 我的 web: https://opendata.sz.gov.cn 申请的appKey
+     */
+    final static String MY_APP_KEY = "";
+    public static void main(String[] args) {
+
+    }
+}

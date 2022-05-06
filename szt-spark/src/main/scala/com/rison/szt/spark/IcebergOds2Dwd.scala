8ebc6d760e4913b55ef381128539fe3b0d5b30a2
Rison_Lee <rison168@163.com>
2022-05-06 17:47:45 +0800

    [feat] ods -> dwd, filter duplicate data

---
+++ szt-spark/src/main/scala/com/rison/szt/spark/IcebergOds2Dwd.scala
@@ -0,0 +1,13 @@
+package com.rison.szt.spark
+
+/**
+ * @PACKAGE_NAME: com.rison.szt.spark
+ * @NAME: IcebergOds2Dwd
+ * @USER: Rison
+ * @DATE: 2022/5/6 17:30
+ * @PROJECT_NAME: iceberg-szt-traffic
+ * */
+
+object IcebergOds2Dwd {
+
+}

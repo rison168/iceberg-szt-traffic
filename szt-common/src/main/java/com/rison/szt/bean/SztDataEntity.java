f4d33f6f4ae688c2b8828295bfc1ccbfa45073ba
Rison_Lee <rison168@163.com>
2022-05-19 11:25:16 +0800

    [feat] field annotation

--- szt-common/src/main/java/com/rison/szt/bean/SztDataEntity.java
+++ szt-common/src/main/java/com/rison/szt/bean/SztDataEntity.java
@@ -14,15 +14,48 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SztDataEntity {
+    /**
+     * 交易时间
+     */
     private String deal_date;
+    /**
+     * 结算时间
+     */
     private String close_date;
+    /**
+     * 卡号
+     */
     private String card_no;
+    /**
+     * 交易金额
+     */
     private String deal_value;
+    /**
+     * 交易类型
+     */
     private String deal_type;
+    /**
+     * 公司名称，线名
+     */
     private String company_name;
+    /**
+     * 列车号
+     */
     private String car_no;
+    /**
+     * 站名
+     */
     private String station;
+    /**
+     * 联程标记
+     */
     private String conn_mark;
+    /**
+     * 实收金额
+     */
     private String deal_money;
+    /**
+     * 闸机号
+     */
     private String equ_no;
 }
